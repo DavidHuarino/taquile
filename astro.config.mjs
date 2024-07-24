be8d207ef3abcbd4a7b9d339f697e603516d5afb
DavidHuarino <ricardohuarino@gmail.com>
2024-07-23 19:53:52 -0500

    add carousel component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    image: {
-        service: {
-            entrypoint: 'astro/assets/services/noop'
-         }
-    }}
-);
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/noop'
+    }
+  },
+  integrations: [react(), tailwind()]
+});
